85aa959a82dbe6e0f277634cb49163319e38d7d8
tuckerreiland <treiland@uw.edu>
2022-07-31 12:06:55 -0400

    filled out models

--- models/Product.js
+++ models/Product.js
@@ -8,10 +8,35 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
+    {
     // define columns
-  },
-  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name:{
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true
+      }
+    }
+    },
+   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
